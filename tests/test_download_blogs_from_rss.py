326ffb9b4affa37e89dd69a9d27f25c4d5c00cbc
JonasBE13 <jonasbengtstrom@gmail.com>
2023-08-23 12:17:36 +0200

    Added test to check if python version is 3.10

--- tests/test_download_blogs_from_rss.py
+++ tests/test_download_blogs_from_rss.py
@@ -1,5 +1,16 @@
+import sys
+
 from newsfeed import download_blogs_from_rss
 
 
 def test_import_project() -> None:
     download_blogs_from_rss.main(blog_name="mit")
+
+
+def test_python_version():
+    major_version = sys.version_info.major
+    minor_version = sys.version_info.minor
+    assert (major_version, minor_version) == (
+        3,
+        10,
+    ), f"Expected Python version 3.10, but got {major_version}.{minor_version}"
