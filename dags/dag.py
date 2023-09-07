340c4413e998fd322bef4cf28edf95626751db54
JonasBE13 <jonasbengtstrom@gmail.com>
2023-09-07 14:44:55 +0200

    added more tasks in airflow

--- dags/dag.py
+++ dags/dag.py
@@ -2,28 +2,73 @@ from datetime import datetime
 
 from airflow.decorators import dag, task
 
-from newsfeed import discord_bot, download_blogs_from_rss, extract_articles, summarize
+from newsfeed import (
+    discord_bot,
+    download_blogs_from_rss,
+    extract_articles,
+    summarize,
+    webscraper,
+)
 
 
+# openai only
+@task(task_id="webscraper")
+def webscraper_task() -> None:
+    webscraper.main()
+
+
+# downnload blogs
 @task(task_id="download_blogs_from_rss")
 def download_blogs_from_rss_task() -> None:
     download_blogs_from_rss.main(blog_name="mit")
-    # download_blogs_from_rss.main(blog_name="aws")
+    download_blogs_from_rss.main(blog_name="aws")
 
 
+# extract articles
 @task(task_id="extract_articles")
 def extract_articles_task() -> None:
     extract_articles.main(blog_name="mit")
+    extract_articles.main(blog_name="aws")
 
 
-@task(task_id="summarize")
-def summarize_task() -> None:
+# technical summaries
+@task(task_id="summarize_tech")
+def summarize_tech_task() -> None:
     summarize.main(source="mit", sum_type="tech")
+    # summarize.main(source="aws", sum_type="tech")
+    summarize.main(source="openai", sum_type="tech")
 
 
+# non-technical summaries
+@task(task_id="summarize_non_tech")
+def summarize_non_tech_task() -> None:
+    summarize.main(source="mit", sum_type="ntech")
+    # summarize.main(source="aws", sum_type="ntech")
+    summarize.main(source="openai", sum_type="ntech")
+
+
+# swedish summaries
+@task(task_id="summarize_swe")
+def summarize_swe_task() -> None:
+    summarize.main(source="mit", sum_type="swe")
+    # summarize.main(source="aws", sum_type="swe")
+    summarize.main(source="openai", sum_type="swe")
+
+
+# discord bot - only sends tech summaries atm, can fix by adding a new task
 @task(task_id="discord_bot")
 def discord_bot_task() -> None:
-    discord_bot.main(source="mit")
+    discord_bot.main(
+        source="mit",
+        ix=1,
+        sum_type="tech",
+    )
+    # discord_bot.main(source="aws", ix=1, sum_type="tech",)
+    discord_bot.main(
+        source="openai",
+        ix=1,
+        sum_type="tech",
+    )
 
 
 @dag(
@@ -38,7 +83,10 @@ def test_pipeline() -> None:
     (
         download_blogs_from_rss_task()
         >> extract_articles_task()
-        >> summarize_task()
+        >> webscraper_task()
+        >> summarize_tech_task()
+        >> summarize_non_tech_task()
+        >> summarize_swe_task()
         >> discord_bot_task()
     )
 
