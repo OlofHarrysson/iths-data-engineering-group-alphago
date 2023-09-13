939102a1b50028755a7de9325a24251f9b892d87
Johan Sandberg <jvesand@gmail.com>
2023-09-13 12:34:02 +0200

    make dashboard less ugly

--- src/newsfeed/dashboard_layout.py
+++ src/newsfeed/dashboard_layout.py
@@ -6,14 +6,72 @@ from dash import dcc, html
 
 class LayoutHandler:
     def __init__(self):
-        self._blog_options = {"mit": "MIT", "aws": "AWS"}
+        self._blog_options = {"mit": "MIT", "aws": "AWS", "openai": "OpenAI"}
         self._prompt_options = {"tech": "Technical", "ntech": "Non-technical", "swe": "Svenska"}
         self._model_options = {"api": "GPT-3.5 (API)", "gpt2": "GPT-2 (local)", "t5": "T5 (local)"}
 
     def create_layout(self):
         return dbc.Container(
             [
-                dbc.Card(dbc.CardBody(html.H1("Blog post summary"))),
+                dbc.Row(
+                    html.H1("Essentify"),
+                    style={"text-align": "center"},
+                    className="mt-3",
+                ),
+                dbc.Row(
+                    html.H3("Blog Summary Dashboard"),
+                    style={"text-align": "center"},
+                    className="mt-1",
+                ),
+                dbc.Row(
+                    className="mt-5",
+                    children=[
+                        dbc.Col(html.H4("Blog"), className="mt-1"),
+                        dbc.Col(html.H4("Summary Type"), className="mt-1"),
+                        dbc.Col(html.H4("Model"), className="mt-1"),
+                    ],
+                ),
+                dbc.Row(
+                    className="mt-0",
+                    children=[
+                        dbc.Col(
+                            dcc.RadioItems(
+                                id="blog-radio",
+                                options=self._blog_options,
+                                value="mit",
+                                style={"display": "flex", "flex-direction": "column"},
+                                inputStyle={"margin-right": "10px"},
+                            )
+                        ),
+                        dbc.Col(
+                            dcc.RadioItems(
+                                id="prompt-radio",
+                                options=self._prompt_options,
+                                value="tech",
+                                style={"display": "flex", "flex-direction": "column"},
+                                inputStyle={"margin-right": "10px"},
+                            )
+                        ),
+                        dbc.Col(
+                            dcc.RadioItems(
+                                id="model-radio",
+                                options=self._model_options,
+                                value="api",
+                                style={"display": "flex", "flex-direction": "column"},
+                                inputStyle={"margin-right": "10px"},
+                            )
+                        ),
+                    ],
+                ),
+                dbc.Row(html.H5("Article"), className="mt-5"),
+                dbc.Row(
+                    dcc.Dropdown(
+                        id="blog-articles-dropdown",
+                        options=[],
+                        style={"width": "100&", "font-size": "1em"},
+                        searchable=True,
+                    )
+                ),
                 dbc.Row(
                     className="mt-4",
                     children=[
@@ -21,46 +79,6 @@ class LayoutHandler:
                             children=[
                                 dbc.Row(dcc.Markdown(id="blog-post")),
                                 dbc.Row(dcc.Markdown(id="link-to-blog-post")),
-                            ]
-                        ),
-                        dbc.Col(
-                            children=[
-                                dbc.Row(html.P("Choose model:"), className="mt-1"),
-                                dbc.Row(
-                                    dcc.RadioItems(
-                                        id="model-radio",
-                                        options=self._model_options,
-                                        value="api",
-                                    )
-                                ),
-                                dbc.Row(html.P("")),
-                                dbc.Row(html.P("Choose type of summary:"), className="mt-1"),
-                                dbc.Row(
-                                    dcc.RadioItems(
-                                        id="prompt-radio",
-                                        options=self._prompt_options,
-                                        value="tech",
-                                    )
-                                ),
-                                dbc.Row(html.P("")),
-                                dbc.Row(html.P("Choose blog:"), className="mt-1"),
-                                dbc.Row(
-                                    dcc.RadioItems(
-                                        id="blog-radio",
-                                        options=self._blog_options,
-                                        value="mit",
-                                    )
-                                ),
-                                dbc.Row(html.P("")),
-                                dbc.Row(html.P("Choose article"), className="mt-1"),
-                                dbc.Row(
-                                    dcc.Dropdown(
-                                        id="blog-articles-dropdown",
-                                        options=[],
-                                        style={"width": "100&", "font-size": "1em"},
-                                        searchable=True,
-                                    )
-                                ),
                             ]
                         ),
                     ],
